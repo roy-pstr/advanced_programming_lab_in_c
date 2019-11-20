16dca288f8b5a441b087ddd92181b63f870593c2
doronrm <doronrachm@gmail.com>
2019-11-20 13:09:29 +0200

    flags.h first steps

---
+++ C_lab_2/params_parser/params_parser.h
@@ -0,0 +1,25 @@
+#ifndef PARAMS_PARSER_H
+#define PARAMS_PARSER_H
+
+#include <stdio.h>
+#include "flags.h"
+
+
+typedef struct Params_st
+{
+	char *sub_str;
+	char *file_in;
+	Flag A;
+	Flag b;
+	Flag c;
+	Flag i;
+	Flag n;
+	Flag v;
+	Flag x;
+	Flag E;
+
+}Params;
+
+void initializeParams(Params *flags);
+
+#endif

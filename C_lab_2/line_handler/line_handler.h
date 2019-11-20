3d7dfad2342f96658a5965b09f59ffcc8f8ec7d8
roy-pstr <49519972+roy-pstr@users.noreply.github.com>
2019-11-20 13:09:59 +0200

    edit main

--- C_lab_2/line_handler/line_handler.h
+++ C_lab_2/line_handler/line_handler.h
@@ -1,6 +1,8 @@
 #ifndef LINE_HANDLER_H
 #define LINE_HANDLER_H
+#include "params_parser.h"
 
 bool isStringInLine(const char *str, const char *line);
 
+void handleLine(char *line, Params params);
 #endif
--- C_lab_2/main.c
+++ C_lab_2/main.c
@@ -1,14 +1,29 @@
+#include <stdbool.h>
+#include <stdio.h>
 #include "line_handler_tests.h"
+#include "line_handler.h"
+#include "params_parser.h"
 
 int main(int argc, char **argv)
 {
-	runLineHandlerTests();
+	char *line = NULL;
+	bool is_file = false;
+	FILE *fp = NULL;
+	Params params;
 
+	//Tests:
+	runLineHandlerTests();
+	runInputHandlerTest();
+
+	//Argument handling:
 	//checkArgv();
 	//parseFlags();
-	/* loop: */
-		//getNextLine(file_flag)
-		//handleOneLine(line, params) //printing function
+	
+	
+	getNextLine(is_file, fp, line);
+	while (line != NULL) {
+		handleLine(line, &params); //printing function.
+	}
 }
 
 //void getLinesAndPrint() {

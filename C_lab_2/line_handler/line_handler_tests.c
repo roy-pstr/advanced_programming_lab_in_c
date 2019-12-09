f2eb72a26ccaf857f5c167da26c4b89f8259273e
doronrm <doronrachm@gmail.com>
2019-12-09 19:50:04 +0200

    added and passes test for the backslash

--- C_lab_2/line_handler/line_handler_tests.c
+++ C_lab_2/line_handler/line_handler_tests.c
@@ -10,19 +10,8 @@
 #define TEST_LINE_MAX_LEN 80
 #define TEST_STR_MAX_LEN  80
 
-void runBasicTest()
+void runBasicTest(const char test_string[][TEST_LINE_MAX_LEN])
 {
-	const char test_string[][TEST_LINE_MAX_LEN] = {
-	   "Every sunday at 4:00 PM,",
-	   "we dive into the C relm.",
-	   "Mozilla is seeking StaffCryptographyEngineer \
-	   to develop featurs for Chrome",
-	   "You believe that test doesn't exist unless it is tested",
-	   "and that tests don't exist unless they are tested.",
-	   "I'm not afraid to copy from my friend.",
-	   "I sometimes have bad jokes.",
-	   "tthis is an important line!"
-	};
 	Params params;
 	initializeParams(&params);
 	rChar regex_str[TEST_STR_MAX_LEN];
@@ -52,19 +41,8 @@ void runBasicTest()
 	assert(isLineMatch(&params, regex_str, test_string[7]));
 }
 
-void runTestWIthV_Arg()
+void runTestWIthV_Arg(const char test_string[][TEST_LINE_MAX_LEN])
 {
-	const char test_string[][TEST_LINE_MAX_LEN] = {
-	   "Every sunday at 4:00 PM,",
-	   "we dive into the C relm.",
-	   "Mozilla is seeking StaffCryptographyEngineer \
-	   to develop featurs for Chrome",
-	   "You believe that test doesn't exist unless it is tested",
-	   "and that tests don't exist unless they are tested.",
-	   "I'm not afraid to copy from my friend.",
-	   "I sometimes have bad jokes.",
-	   "tthis is an important line!"
-	};
 	Params params;
 	initializeParams(&params);
 	onFlag(&params.v);
@@ -95,8 +73,50 @@ void runTestWIthV_Arg()
 	assert(!isLineMatch(&params, regex_str, test_string[7]));
 }
 
+void runTestWithEscapes(const char test_string[][TEST_LINE_MAX_LEN])
+{
+	Params params;
+	initializeParams(&params);
+	onFlag(&params.E);
+	rChar regex_str[TEST_STR_MAX_LEN];
+	putRegex(regex_str, "\\[4:00\\]");
+	assert(isLineMatch(&params, regex_str, test_string[0]));
+	putRegex(regex_str, "\\[");
+	assert(isLineMatch(&params, regex_str, test_string[0]));
+	putRegex(regex_str, "\\[3");
+	assert(!isLineMatch(&params, regex_str, test_string[0]));
+	putRegex(regex_str, "C relm\\.");
+	assert(isLineMatch(&params, regex_str, test_string[1]));
+	putRegex(regex_str, "Chrome");
+	assert(isLineMatch(&params, regex_str, test_string[2]));
+	putRegex(regex_str, "\\.");
+	assert(!isLineMatch(&params, regex_str, test_string[3]));
+	putRegex(regex_str, "I'm not afraid");
+	assert(isLineMatch(&params, regex_str, test_string[5]));
+	putRegex(regex_str, "I sometimes have bad jokes\\.\\.\\.");
+	assert(isLineMatch(&params, regex_str, test_string[6]));
+	putRegex(regex_str, "I sometimes have bad jokes\\.\\.\\.\\.");
+	assert(!isLineMatch(&params, regex_str, test_string[6]));
+	putRegex(regex_str, "\\(!\\)");
+	assert(isLineMatch(&params, regex_str, test_string[7]));
+}
+
+
 void runLineHandlerTests()
 {
-	runBasicTest();
-	runTestWIthV_Arg();
+	const char test_string[][TEST_LINE_MAX_LEN] = {
+   "Every sunday at [4:00] PM,",
+   "we dive into the C relm.",
+   "Mozilla is seeking StaffCryptographyEngineer \
+	to develop featurs for Chrome",
+	"You believe that test doesn't exist unless it is tested",
+	"and that tests don't exist unless they are tested.",
+	"I'm not afraid to copy from my friend.",
+	"I sometimes have bad jokes...",
+	"tthis is an important line(!)"
+	};
+
+	runBasicTest(test_string);
+	runTestWIthV_Arg(test_string);
+	runTestWithEscapes(test_string);
 }
